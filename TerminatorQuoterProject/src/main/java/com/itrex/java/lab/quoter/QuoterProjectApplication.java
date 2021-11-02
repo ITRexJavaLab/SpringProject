827c996fefd14be9bbf1900bb3232860a2adb7cc
Maxim Babrovich <v-max.b@turvo.com>
2021-11-03 01:00:49 +0300

    init TerminatorQuoterProject

---
+++ TerminatorQuoterProject/src/main/java/com/itrex/java/lab/quoter/QuoterProjectApplication.java
@@ -0,0 +1,25 @@
+package com.itrex.java.lab.quoter;
+
+public class QuoterProjectApplication {
+
+    public static void main(String[] args) {
+//        xmlConfig();
+//        annotationConfig();
+//        javaConfig();
+//        javaConfigProperties();
+    }
+
+    public static void xmlConfig() {
+    }
+
+    public static void annotationConfig() {
+    }
+
+    public static void javaConfig() {
+    }
+
+    public static void javaConfigProperties() {
+    }
+}
+
+
